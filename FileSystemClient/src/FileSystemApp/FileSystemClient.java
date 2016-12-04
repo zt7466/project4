9e183a313204c58e786f0c1eba46c5829fa7d05e
zt7466 <zt7466@ship.edu>
2016-12-04 13:59:03 -0500

    Initial commit of Project 3 stuff as this project is going to rely heavily on it

---
+++ FileSystemClient/src/FileSystemApp/FileSystemClient.java
@@ -0,0 +1,114 @@
+package FileSystemApp;
+
+import org.omg.CosNaming.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import org.omg.CORBA.*;
+
+/**
+ * A simple client that just gets a
+ * @author Merlin
+ *
+ */
+public class FileSystemClient
+{
+	static FileSystem fileSystemImpl;
+
+	private final static int RUN_NUMBER = 41;
+	/**
+	 * Just do each operation once
+	 * @param args ignored
+	 */
+	public static void main(String args[])
+	{
+		try
+		{
+			// create and initialize the ORB
+			ORB orb = ORB.init(args, null);
+
+			// get the root naming context
+			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+			// Use NamingContextExt instead of NamingContext. This is
+			// part of the Interoperable naming Service.
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+			// resolve the Object Reference in Naming
+			String name = "FileSystem";
+			fileSystemImpl = FileSystemHelper.narrow(ncRef.resolve_str(name));
+
+			System.out.println("Obtained a handle on server object: " + fileSystemImpl);
+			System.out.println(fileSystemImpl.sayHello());
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File("20kb.txt")));
+			for(int i = 0; i < RUN_NUMBER; i++)
+			{
+			Long startTime = System.nanoTime();
+			fileSystemImpl.readFile("test1.txt");
+			Long endTime = System.nanoTime();
+			System.out.println("Finished run: " + i);
+
+			writer.write((endTime - startTime)+ "\n");
+			}
+			writer.close();
+
+			writer = new BufferedWriter(new FileWriter(new File("40kb.txt")));
+			for(int i = 0; i < RUN_NUMBER; i++)
+			{
+			Long startTime = System.nanoTime();
+			fileSystemImpl.readFile("test2.txt");
+			Long endTime = System.nanoTime();
+			System.out.println("Finished run: " + i);
+
+			writer.write((endTime - startTime)+ "\n");
+			}
+			writer.close();
+
+			writer = new BufferedWriter(new FileWriter(new File("60kb.txt")));
+			for(int i = 0; i < RUN_NUMBER; i++)
+			{
+			Long startTime = System.nanoTime();
+			fileSystemImpl.readFile("test3.txt");
+			Long endTime = System.nanoTime();
+			System.out.println("Finished run: " + i);
+
+			writer.write((endTime - startTime)+ "\n");
+			}
+			writer.close();
+
+			writer = new BufferedWriter(new FileWriter(new File("80kb.txt")));
+			for(int i = 0; i < RUN_NUMBER; i++)
+			{
+			Long startTime = System.nanoTime();
+			fileSystemImpl.readFile("test4.txt");
+			Long endTime = System.nanoTime();
+			System.out.println("Finished run: " + i);
+
+			writer.write((endTime - startTime)+ "\n");
+			}
+			writer.close();
+
+			writer = new BufferedWriter(new FileWriter(new File("120kb.txt")));
+			for(int i = 0; i < RUN_NUMBER; i++)
+			{
+			Long startTime = System.nanoTime();
+			fileSystemImpl.readFile("test5.txt");
+			Long endTime = System.nanoTime();
+			System.out.println("Finished run: " + i);
+
+			writer.write((endTime - startTime)+ "\n");
+			}
+			writer.close();
+			// This is how we would shut down the server
+			//fileSystemImpl.shutdown();
+
+		} catch (Exception e)
+		{
+			System.out.println("ERROR : " + e);
+			e.printStackTrace(System.out);
+		}
+	}
+
+}
